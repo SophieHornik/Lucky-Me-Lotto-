f986566e1970da67fd101a3b57a4e9864be585cd
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-03-22 18:34:16 +0000

    saving before loops

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const nameArray=[] //create a list to store names
 
 function addName(){
 //get a trimmed version of the name from input box
-const name = document.getElementById ('nameInput').value.trim()
+const nameInput = document.getElementById('nameInput')
+const name = nameInput ('nameInput').value.trim()
 //adds to the end of list)
-nameArray.push(name) 
+nameArray.push(name)
+displayNames()
 }
+
+function displayNames() {
+    
+}
+
 document.getElementById('addNameBtn').addEventListener('click', addName)
