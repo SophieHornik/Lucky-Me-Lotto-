22c8203feffbdc6af73464fdff60ef951e04bf95
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-03-22 18:57:52 +0000

    loop html

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ function displayNames() {
     nameList.innerHTML='' //clears list
         //i is counter variable that counts how many times we do loop
     for (let i=0; i < nameArray.length; i++) {
-
-    }
-    
-
-
+const name = nameArray[i] //get current name from array
+const li = document.createElement('li')
+li.className = 'list-group-item'
+   
+    const span = document.createElement('span')
+    span.textContent = name
+    li.appendChild(span)
+nameList.appendChild(li)
 }
-
+ }
 document.getElementById('addNameBtn').addEventListener('click', addName)
