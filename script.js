ac83ff01511f7f6f5446a3474fedbd4ed0545253
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-03-26 02:52:31 +0000

    fixed issue

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const nameArray=[] //create a list to store names
+const nameArray = [] //Create a list to store names
 
+function addName () {
+  //get a trimmed version of the name from the input box
+  const nameInput = document.getElementById('nameInput')
+  const name = nameInput.value.trim()
+  nameArray.push(name)
+  displayNames()
+  nameInput.value = ''
+}
+function displayNames(){
+    const nameList = document.getElementById('nameList')//get UL element 
+    nameList.innerHTML = '' //clears the list 
 
-function addName(){
-//get a trimmed version of the name from input box
-const nameInput = document.getElementById('nameInput')
-const name = nameInput ('nameInput').value.trim()
-//adds to the end of list)
-nameArray.push(name)
-displayNames()
+    for (let i = 0; i < nameArray.length; i++){
+        const name = nameArray[i] //get the current name from the array
+
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)
+        nameList.appendChild(li)
+
+    }
+
 }
 
-function displayNames() {
-    const nameList = document.getElementById('nameList') //get ul (list) element
-    nameList.innerHTML='' //clears list
-        //i is counter variable that counts how many times we do loop
-    for (let i=0; i < nameArray.length; i++) {
-const name = nameArray[i] //get current name from array
-const li = document.createElement('li')
-li.className = 'list-group-item'
-   
-    const span = document.createElement('span')
-    span.textContent = name
-    li.appendChild(span)
-nameList.appendChild(li)
-}
- 
+
+
+
+
+
+
+// add onclick to add name btn
 document.getElementById('addNameBtn').addEventListener('click', addName)
